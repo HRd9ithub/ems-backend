4cad28e143fce23dfd73d78b6e5c9a417d3320cc
hardik <hardik.d9ithub@gmail.com>
2023-12-22 16:51:06 +0530

    gst and account deatil for invoice changes

--- src/models/invoiceAccountSchema.js
+++ src/models/invoiceAccountSchema.js
@@ -1,38 +1,48 @@
-const { model, Schema } = require("mongoose");
+const { model, Schema, default: mongoose } = require("mongoose");
+const decryptData = require("../helper/decryptData");
+
+function invoiceAccountDataDecrypt(data) {
+    return decryptData(data);
+}
+
+mongoose.set('toJSON', { getters: true });
 
 const invoiceAccountSchema = new Schema({
     bank: {
-        type :String,
-        required : true
+        type: String,
+        required: true,
+        get: invoiceAccountDataDecrypt
     },
-    account_number :{ 
-        type :String,
-        required : true
+    account_number: {
+        type: String,
+        required: true,
+        get: invoiceAccountDataDecrypt
     },
-    ifsc_code:{
-        type : String,
-        required : true
+    ifsc_code: {
+        type: String,
+        required: true,
+        get: invoiceAccountDataDecrypt
     },
-    branch_name : {
-        type : String,
-        required : true
+    status: {
+        type: Boolean,
+        required: true,
+        default: true
     },
-    name : {
-        type : String,
-        required : true
+    branch_name: {
+        type: String,
+        required: true,
+        get: invoiceAccountDataDecrypt
     },
-    invoice_id : {
-        type : Schema.Types.ObjectId,
-        ref: "invoice",
-        required : true
-    },
+    name: {
+        type: String,
+        required: true,
+        get: invoiceAccountDataDecrypt
+    }
 },
     {
         timestamps: true
-    }
+    }, { toJSON: { getters: true } }
 );
-
-`id`, `bank`, `country`, `accountnumber`, `IBAN`, `SWIFT`, `HName`, `phone`, `invoiceId`, `loginId`, `Deleted_account`
 
 const invoice_account = new model("invoice_account", invoiceAccountSchema);
 
