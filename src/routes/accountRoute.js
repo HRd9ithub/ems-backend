e572e6f4d2a2e7b4791717d25017e171e9667b5b
hardik <hardik.d9ithub@gmail.com>
2023-12-27 14:37:41 +0530

    invoice controller changes and validation

--- src/routes/accountRoute.js
+++ src/routes/accountRoute.js
@@ -6,20 +6,12 @@ const accountRoute = express.Router();
 
 
 let accountValidation = [
-    check('bank_name', "Bank name field is required.").notEmpty(),
+    check('bank_name', "Bank name is a required field.").notEmpty(),
     check('user_id', "User id is Required.").isMongoId(),
-    check('name', "Name field is required.").notEmpty(),
-    check('branch_name', "Branch name field is required.").notEmpty(),
-    check("account_number", "Account number is Required.").notEmpty().custom(async (account_number, { req }) => {
-        if (account_number && account_number.toString().length < 12) {
-            throw new Error('Account number must be at least 12 character.')
-        }
-    }),
-    check("ifsc_code", "IFSC code is Required.").notEmpty().custom(async (ifsc_code, { req }) => {
-        if (ifsc_code && ifsc_code.toString().length !== 11) {
-            throw new Error('IFSC code must be at least 11 character.')
-        }
-    })
+    check('name', "Name is a required field.").notEmpty(),
+    check('branch_name', "Branch name is a required field.").notEmpty(),
+    check("account_number", "Account number is a required field.").notEmpty(),
+    check("ifsc_code", "IFSC code is a required field.").notEmpty()
 ]
 
 
