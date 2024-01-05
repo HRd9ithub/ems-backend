39a4b6bbf062f349e18c1623feb61356c43c4286
hardik <hardik.d9ithub@gmail.com>
2024-01-05 10:38:47 +0530

    cron-job timezone change testing

--- src/cron-job.js
+++ src/cron-job.js
@@ -10,60 +10,65 @@ const ejs = require('ejs');
 const sendBirthdayMail = async () => {
     const { SMTP_EMAIL, SMTP_PASSWORD } = process.env
 
+    // Set the timezone to Asia/Kolkata
+    const timeZone = 'Asia/Kolkata';
+
     try {
-        cron.schedule("30 10 * * *", async () => {
-            const userData = await user.find({ date_of_birth: { $exists: true } }, { first_name: 1, last_name: 1, date_of_birth: 1, _id: 0 });
-            const birthList = userData.filter((val) => {
-                return moment(val.date_of_birth).format("DD-MM") === moment(new Date()).format("DD-MM")
-            })
-
-            if (birthList.length !== 0) {
-                const adminMailArray = await getAdminEmail();
-    
-                const adminMail = adminMailArray.map((val) => val.email);
-
-                const birthListData = birthList.map((val) =>{
-                    return { name : val?.first_name.concat(" ",val?.last_name)}
+        const birthCronJob = cron.schedule("45 10 * * *", async () => {
+                const userData = await user.find({ date_of_birth: { $exists: true } }, { first_name: 1, last_name: 1, date_of_birth: 1, _id: 0 });
+                const birthList = userData.filter((val) => {
+                    return moment(val.date_of_birth).format("DD-MM") === moment(new Date()).format("DD-MM")
                 })
-    
-                const transporter = nodemailer.createTransport({
-                    host: 'smtp.gmail.com',
-                    port: 587,
-                    auth: {
-                        user: SMTP_EMAIL,
-                        pass: SMTP_PASSWORD
-                    }
-                });
+
+                if (birthList.length !== 0) {
+                    const adminMailArray = await getAdminEmail();
         
-                // get file path
-                const filepath = path.resolve(__dirname, "../views/birthdayTemplate.ejs");
+                    const adminMail = adminMailArray.map((val) => val.email);
+
+                    const birthListData = birthList.map((val) =>{
+                        return { name : val?.first_name.concat(" ",val?.last_name)}
+                    })
         
-                // read file using fs module
-                const htmlstring = fs.readFileSync(filepath).toString();
-                // add data dynamic
-                const content = ejs.render(htmlstring, { employeeList : birthListData });
-        
-        
-                const from = `D9ithub <${SMTP_EMAIL}>`
-                const mailOptions = {
-                    from: from,
-                    to: adminMail,
-                    subject: "Birthday Announcement",
-                    html: content
-                };
-        
-                transporter.sendMail(mailOptions,(error,info) => {
-                    if (error) {
-                        console.log('Error sending birthday notification email:', error);
-                    } else {
-                        console.log('Birthday notification email sent:', info.response);
-                    }
-                })
-            }
-        }, {
-            scheduled: true,
-            timezone: "Asia/Kolkata"
-        });
+                    const transporter = nodemailer.createTransport({
+                        host: 'smtp.gmail.com',
+                        port: 587,
+                        auth: {
+                            user: SMTP_EMAIL,
+                            pass: SMTP_PASSWORD
+                        }
+                    });
+            
+                    // get file path
+                    const filepath = path.resolve(__dirname, "../views/birthdayTemplate.ejs");
+            
+                    // read file using fs module
+                    const htmlstring = fs.readFileSync(filepath).toString();
+                    // add data dynamic
+                    const content = ejs.render(htmlstring, { employeeList : birthListData });
+            
+            
+                    const from = `D9ithub <${SMTP_EMAIL}>`
+                    const mailOptions = {
+                        from: from,
+                        to: adminMail,
+                        subject: "Birthday Announcement",
+                        html: content
+                    };
+            
+                    transporter.sendMail(mailOptions,(error,info) => {
+                        if (error) {
+                            console.log('Error sending birthday notification email:', error);
+                        } else {
+                            console.log('Birthday notification email sent:', info.response);
+                        }
+                    })
+                }
+            }, {
+                timezone: timeZone
+            });
+
+        // Start the cron job
+        birthCronJob.start();
     } catch (error) {
         console.log(error, "catch-error");
     }
